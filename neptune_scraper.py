90ee4d1b491af6cb32e4af1625b6f606cdf4aa08
lasisitaofeek <38340719+lasisitaofeek@users.noreply.github.com>
2018-04-18 12:05:22 +0100

    neptunescraper.py
    Now enhanced to scrape all neptune website paginated pages. The ScrapeWeb function now takes the base url for the NEXT link without the page and the pageSize querystring, and a callback function that does the specific scraping. Example is provided for the cigar sampler page.
    
    This function accepts the base URL, as explained above, the callback fn, and a maximum number of allowed html download errors before ending the scraping job.
    
    Please, let me have your observations.

--- neptune_scraper.py
+++ neptune_scraper.py
@@ -19,6 +19,7 @@ CigarModel = {
             "price": 0.00,
             "profile": "",
             "rating": 0.0,
+            "ratingCount": 0,
             "shapes": "",
             "smoking-notes": "",
             "strength": "",
@@ -28,15 +29,28 @@ CigarModel = {
 cigars = []
 #kount = 0
 
-def ProcessCigarItem(cigarItemTag):
+def ProcessCigarSamplerItem(cigarItemTag):
     cigar = CigarModel
-    #print(cigar)
+    try:
+        name = cigarItemTag.find(itemprop="name").text
+    except:
+        name = ""
 
-    name = cigarItemTag.find(itemprop="name").text
-    productURL = cigarItemTag.find(itemprop="url").get("content")
-    notes = cigarItemTag.find("div", {"class": "listing_descriptions"}).text
-    photo = cigarItemTag.find(itemprop="image").get("content")
+    try:
+        productURL = cigarItemTag.find(itemprop="url").get("content")
+    except:
+        productURL = ""
+    
+    try:
+        notes = cigarItemTag.find("div", {"class": "listing_descriptions"}).text
+    except:
+        notes = ""
 
+    try:
+        photo = cigarItemTag.find(itemprop="image").get("content")
+    except expression as identifier:
+        photo = ""
+    
     try:
         productID = cigarItemTag.find("button", {"class": "pr_ddBtn"}).get("id")
     except:
@@ -48,61 +62,47 @@ def ProcessCigarItem(cigarItemTag):
     try:
         rating = cigarItemTag.find(itemprop="ratingValue").get("content")
     except:
-        rating = 0
+        rating = 0.0
 
+    try:
+        ratingCount = cigarItemTag.find(itemprop="ratingCount").get("content")
+    except:
+        ratingCount = 0.0
+    
     try:
         price = cigarItemTag.find(itemprop="price").get("content")
     except:
-        price = 0
+        price = 0.00
 
-    #print(rating)
-    #""""
     cigar["name"] = name
     cigar["photos"] = photo
     cigar["notes"] = notes
     cigar["rating"] = rating
+    cigar["ratingCount"] = ratingCount  #number of raters
     cigar["price"]  = price
-    cigar["productID"] = productID
+    cigar["productID"] = productID  
     cigar["productURL"] = productURL
-    #print(cigar)
+
+    # you should comment out the following line in production releasse
+    # print(cigar)
+
     cigars.append(cigar)
     #"""
 
-def ScrapeWeb(webURL, kount):
-    #request = loadweb.LoadWeb(("http://neptune.cigar:7070")
-    request = loadweb.LoadWeb(webURL)
-    soup = BeautifulSoup(request.content, 'html.parser')
-
-    for prod in soup.findAll("div", {"class": "product_item"}):
-        ProcessCigarItem(prod)
-
-    kount = kount + 2
-    searchPart = "%s&amp;pg=%s" % (1000, kount)  # (str(1000), str(kount))
-    strHref = "https://www.neptunecigar.com/search?text=cigar%20list&amp;nb=" + searchPart
-    try:
-        ScrapeWeb(strHref, kount)
-    except:
-        return
-   
-if __name__ == "__main__":
-    """
-    try:
-        searchPart = "%s&amp;pg=%s" % (40, kount)  # (str(1000), str(kount))
-        strHref = "https://www.neptunecigar.com/search?text=cigar%20list&amp;nb=" + searchPart
-        ScrapeWeb(strHref, kount)
-        except:
-            print("Oooh! Some error")
-        finally:
-            print(cigars)
-    """
-
-    # maximum number of consecutive download errors allowed
-    max_errors = 5
+"""
+    ScrapeWeb(url, max_errors=3)
+    url - the base paging URL, without the querystring for pageSize and page
+    
+    max_errors - maximum number of consecutive download errors allowed
+    call_back - a function to parse a specific page (e.g. ProcessCigarSamplerItem above that parses the cigar sampler page)
+                with this, we can now scrape other pages filtered by manufacturer, origin, price, etc.
+"""
+def ScrapeWeb(baseUrl, call_back=ProcessCigarSamplerItem, max_errors=3):
     # current number of consecutive download errors
     num_errors = 0
     for page in itertools.count(1):
-        searchPart = "%d&amp;pg=%d" % (12, page)
-        url = 'https://www.neptunecigar.com/search?text=cigar%20list&amp;nb=' + searchPart
+        searchPart = "nb=%d&pg=%d" % (48, page)
+        url = baseUrl + searchPart
         html = loadweb.LoadWeb(url)
         if html is None:
             # received an error trying to download this webpage
@@ -116,8 +116,23 @@ if __name__ == "__main__":
             # ...
             soup = BeautifulSoup(html.content, 'html.parser')
             for prod in soup.findAll("div", {"class": "product_item"}):
-                ProcessCigarItem(prod)
+                #ProcessCigarSamplerItem(prod)
+                call_back(prod)
 
             num_errors = 0
+        
+        # to quickly return for a test scenario
+        # please, enable the comment for production
+        #"""
+        if page == 20:
+            break
+        #"""
+   
+if __name__ == "__main__":
+    #TO DO: Convert the list to a JSON doc, instead of print as the code below does
+    try:
+        ScrapeWeb('https://www.neptunecigar.com/search?text=cigar list&;', ProcessCigarSamplerItem, 3)
+    except:
+        pass
 
     print(cigars)
